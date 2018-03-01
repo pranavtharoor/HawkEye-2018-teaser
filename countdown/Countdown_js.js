7c265d14949ea1b302425580107d6387d2daded0
Aritra Bandyopadhyay <aritraban10@gmail.com>
2018-03-01 23:21:40 +0530

    positioning divs and making them respomnsive

--- countdown/Countdown_js.js
+++ countdown/Countdown_js.js
@@ -1,22 +1,16 @@
 
 function main()
 {
-	//alert("yes");
-	//alert("1");
 	var start = setInterval(timer,1000);
-	//alert("yes");
 }
 function timer()
 {
-	var zero_hour = new Date("2018-3-25T00:00:00+05:30");
-	var today = new Date().getTime();
-
-	var difference = zero_hour - today;
-	//var yes = new Date();
-	//alert(yes.getHours());
-	//alert("1");
+	var zero_hour = new Date("2018-3-25T00:00:00+05:30");		// date and time when countdown becomes zero
+	var today = new Date().getTime();							//date and time on which browser is opened by anyone
+	var going_up_date = new Date("2018-3-20T00:00:00+05:30");// date and time on which countdown webpage vgoes up
+	var time_total =zero_hour - going_up_date;				// (IMPORT) total time for hourglass to go from full to empty in MILLISECONDS 
+	var difference = zero_hour - today;						// (IMPORT) time left for hourglass to become empty from time when webpage is opened by anyone in MILLISECONDS 
 	var days = Math.floor(difference/(1000*60*60*24));
-	//alert("2");
 	var hours=Math.floor((difference%(1000*60*60*24))/(1000*60*60));
 	var minutes=Math.floor((difference%(1000*60*60))/(1000*60));
 	var seconds =Math.floor((difference%(1000*60))/(1000));
