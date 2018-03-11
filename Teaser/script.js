27627f1f9a7fee9ae02e2f9bc53e840ab142e23e
Anshita <anshtita_b@yahoo.co.in>
2018-03-12 00:18:40 +0530

    overlapping cards

--- Teaser/script.js
+++ Teaser/script.js
@@ -1,18 +1,59 @@
-console.log(11);
-
 window.onload=function()
 {
   
   var card = document.getElementsByClassName("card");
-
   for (var i = 0; i < card.length; i++) 
   {
     
     card[i].addEventListener('click', function(){
 
       this.classList.toggle("flipped");
+
+
+
     });
-      
   }
 
-};
+  //if i remove the code from here onwards, each card will flip when clicked. otherwise
+  //some cards are not flipped on clicking
+
+   var x = window.matchMedia("(max-width: 632px)");
+  onSizeChange(x) ;
+  x.addListener(onSizeChange);
+  function onSizeChange(x) {
+    if (x.matches) 
+    {
+    // If media query matches
+       for (var i = 0; i < card.length; i++) 
+        {
+
+            card[i].style.zIndex=i+1;
+            card[i].style.top=-100*i+'px';
+        }
+          for (var i = 0; i < card.length; i++) {
+            card[i].addEventListener('click', separateCards);
+          
+        }
+    
+    }
+  }
+  
+ 
+
+  function separateCards()
+  {
+
+    for (var i = 0; i < card.length; i++) 
+    {
+            card[i].style.top=100*i+'px';
+            card[i].style.boxShadow="none";
+    }
+  }
+}
+
+
+
+  
+
+
+
